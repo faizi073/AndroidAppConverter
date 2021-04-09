8021962ed1029886c3f8f2a0358276420b20fbc0
faizan ali <codeexpert073@gmail.com>
2021-04-09 22:23:56 +0500

    Final13

--- src/app.js
+++ src/app.js
@@ -1,60 +1,23 @@
-const path = require('path')
-const express = require('express')
-const multer = require('multer')
-const fs= require('fs')
 
-
+const express = require('express');
+const app = express();
 const debug = require('debug')('myapp:server');
+const path = require('path');
+const multer = require('multer');
 const logger = require('morgan');
 const serveIndex = require('serve-index')
-
-const app = express()
-
-
-let filename;
-
-
-const {convertWordFiles,} = require("convert-multiple-files");
-
-//set path of views and static assests
-const publicDirectoryPath = path.join(__dirname, '../public')
-
-//Use static assests like css,client side js, images
-app.use(express.static(publicDirectoryPath))
-
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, './public/uploads')
     },
     filename: (req, file, cb) => {
-        filename=(file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-        cb(null, filename)
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
 });
 
-
-
-
-
-  
-
-
 //will be using this for uplading
-
-const upload = multer({
-    limits: {
-        fileSize: 10000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx|ppt|pdf)$/)) {
-            return cb(new Error('Please upload an image'))
-        }
-
-        cb(undefined, true)
-    },
-    storage
-})
+const upload = multer({ storage: storage });
 
 
 app.use(logger('tiny'));
@@ -64,45 +27,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/ftp', express.static('public'), serveIndex('public', {'icons': true}));
 
 app.get('/', function(req,res) {
-    return res.send("hello BRO")
+    return res.send("hello from my app express server!")
 })
 
-
-app.post('/doc',upload.single('file'),async (req,res)=>
-{
-    try {
-
-
-        console.log(path.join(__dirname,`../public/uploads/${filename}`) + "  Y1122")
-
-        console.log(__filename + "HERE")
-        await convertWordFiles(path.join(__dirname,`../public/uploads/${filename}`), 'pdf', path.join(__dirname,'../public/converted'))
-
-
-        PDFFileName = filename.substr(0, filename.lastIndexOf(".")) + ".pdf";
-
-        FTPPath= (req.hostname  + `/FTP/converted/${PDFFileName}`);
-
-        console.log(FTPPath + "YOO")
-
-        res.send(FTPPath);
-
-    }catch (error) {
-
-        console.log(error + "HERE")
-          res.send(__filename + "  ///  "+ __dirname )
-    } 
-
-
+app.post('/doc', upload.single('file'), function(req,res) {
+    debug(req.file);
+    console.log('storage location is ', req.hostname +'/' + req.file.path);
+    return res.send(req.file);
 })
-
-
-
-
-
-
-
-
 
 
 const port = process.env.PORT || 3000;
